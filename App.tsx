46ff1bd61f3abda091654964c2869550c80eca6d
obates <olibates@live.com>
2021-04-22 11:53:31 +0100

    Initial styling

--- App.tsx
+++ App.tsx
@@ -1,18 +1,40 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import arrow from "./assets/arrow.png";
+import spinner from "./assets/spinner.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome, welcome, one and all!</Text>
+      <Text style={styles.header}>SPIN TO WIN</Text>
+      <Image source={arrow} style={styles.arrow} />
+      <Image source={spinner} style={styles.spinner} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    color: "#fff",
+    fontSize: 94,
+    textAlign: "center",
+    fontWeight: "bold",
+    letterSpacing: -4,
+    textShadowColor: "#fff",
+    textShadowRadius: 6,
+  },
+  arrow: {
+    width: 52,
+    height: 52,
+  },
+  spinner: {
+    width: 440,
+    height: 440,
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
   },
